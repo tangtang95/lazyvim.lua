9a45f6f20e46d9b258cabe39488fbfe259710fbb
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-03 12:23:37 +0100

    chore: disable tokyonight plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
 }
