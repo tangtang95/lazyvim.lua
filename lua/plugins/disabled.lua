2d777f767de65b90674995ccc7a5696e4b00ee15
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-03 12:05:13 +0100

    chore: replace neotree with nvim-tree

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  -- { "folke/noice.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
 }
