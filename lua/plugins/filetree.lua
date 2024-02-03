cf0da6fc3a80b87b293745de4936f72d6a4c7c8d
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-03 16:41:40 +0100

    fix: nvim-tree keymaps

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,5 +1,5 @@
 return {
-  -- TODO: improve lazy load of nvim-tree to save about 20 ms when not loading a file tree 
+  -- TODO: improve lazy load of nvim-tree to save about 20 ms when not loading a file tree
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
@@ -7,8 +7,17 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("nvim-tree.api").tree.toggle()
+        end,
+        { desc = "Toggle NvimTree" },
+      },
+    },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup({})
     end,
   },
 }
