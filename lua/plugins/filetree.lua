b08d66779325e2e74c8cc204ea8dfd1a18e86a24
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-04 10:17:24 +0100

    chore: remove projects plugin

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -42,18 +42,4 @@ return {
       })
     end,
   },
-  {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup()
-      require("telescope").load_extension("projects")
-    end,
-    keys = {
-      {
-        "<leader>sp",
-        ":Telescope projects<CR>",
-        desc = "Find Recent Projects",
-      },
-    },
-  },
 }
