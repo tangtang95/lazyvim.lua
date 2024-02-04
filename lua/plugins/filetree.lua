efd60e582595819058f89efb78b26d02b384004e
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-04 11:59:29 +0100

    chore: replace nvim-tree with oil plugin

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,44 +1,27 @@
 return {
-  -- TODO: improve lazy load of nvim-tree to save about 20 ms when not loading a file tree
   {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
+    "stevearc/oil.nvim",
     lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
       {
         "<leader>e",
-        function()
-          require("nvim-tree.api").tree.toggle()
-        end,
-        desc = "Toggle NvimTree",
+        "<CMD>Oil<CR>",
+        desc = "Open Oil Explorer",
       },
     },
     config = function()
-      local function on_attach(bufnr)
-        local api = require("nvim-tree.api")
-
-        local function opts(desc)
-          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-
-        -- default mappings
-        api.config.mappings.default_on_attach(bufnr)
-
-        -- custom mappings
-        vim.keymap.set("n", "w", api.tree.change_root_to_node, opts("CD"))
-        vim.keymap.set("n", "s", api.tree.change_root_to_parent, opts("Up"))
-      end
-      require("nvim-tree").setup({
-        sync_root_with_cwd = true,
-        respect_buf_cwd = true,
-        update_focused_file = {
-          enable = true,
-          update_root = true,
+      require("oil").setup({
+        keymaps = {
+          ["<leader>e"] = "<Nop>",
+          ["`"] = false,
+          ["~"] = false,
+          ["<C-k>"] = "actions.cd",
+          ["<C-w>"] = "actions.tcd",
         },
-        on_attach = on_attach
+        view_options = {
+          show_hidden = true,
+        },
       })
     end,
   },
