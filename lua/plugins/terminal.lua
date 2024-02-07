70fa28c427027407d07023696c31d88379bd92d7
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-07 19:17:15 +0100

    chore: improve wezterm integration

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -5,16 +5,39 @@ return {
     cmd = "WeztermSpawn",
     keys = {
       {
-        "<leader>wt",
+        "<leader>tt",
         function()
-          require("wezterm").spawn("cmd", {
+          local cwd = vim.loop.cwd() -- TODO: migrate when neovim v0.10
+          local args = { "cli", "spawn", "--cwd", cwd }
+          local msg = "spawn " .. table.concat(args, " ")
+          require("wezterm").exec(args, function(obj)
+            ---@diagnostic disable-next-line: undefined-field
+            if obj.code ~= 0 then
+              vim.notify("Wezterm failed to " .. msg, vim.log.levels.ERROR, {
+                title = "Wezterm",
+              })
+            end
+          end)
+        end,
+        desc = "Open new terminal shell",
+      },
+      {
+        "<leader>tv",
+        function()
+          require("wezterm").split_pane.horizontal({
             cwd = vim.loop.cwd(),
-            args = {
-              "/k", "%CMDER_ROOT%\\vendor\\init.bat"
-            }
           })
         end,
-        desc = "Open terminal",
+        desc = "Split vertically new terminal shell",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("wezterm").split_pane.vertical({
+            cwd = vim.loop.cwd(),
+          })
+        end,
+        desc = "Split horizontally new terminal shell",
       },
     },
   },
