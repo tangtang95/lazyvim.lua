1b5f54f98d0f97612edba898c19f37dd985535a8
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-09 21:36:01 +0100

    chore: make git plugins very lazy

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,11 +1,11 @@
 local ssh_private_gitlab_domain = vim.env.SSH_PRIVATE_GITLAB_DOMAIN
 local https_private_gitlab_domain = vim.env.HTTPS_PRIVATE_GITLAB_DOMAIN
 if ssh_private_gitlab_domain ~= nil and https_private_gitlab_domain ~= nil then
-  vim.g.fugitive_gitlab_domains = {[ ssh_private_gitlab_domain ] = https_private_gitlab_domain}
+  vim.g.fugitive_gitlab_domains = { [ssh_private_gitlab_domain] = https_private_gitlab_domain }
 end
 
 return {
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
-  "shumphrey/fugitive-gitlab.vim"
+  { "tpope/vim-fugitive", event = "VeryLazy" },
+  { "tpope/vim-rhubarb", event = "VeryLazy" },
+  { "shumphrey/fugitive-gitlab.vim", event = "VeryLazy" },
 }
