a4d9ec4d084f6aaa6f00db0a4536da91e8d99906
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-09 18:30:49 +0100

    chore: add extra markdown and fugitive gitlab

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,6 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "plugins" },
   },
   defaults = {
