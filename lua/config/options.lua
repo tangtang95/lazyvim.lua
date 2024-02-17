7799aeb4b4a36db0dde42fdbded1e3620722a10c
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-17 16:29:52 +0100

    chore: disable lang providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,11 @@
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
+--- preferences
 vim.g.autoformat = false
 vim.opt.relativenumber = false
 
