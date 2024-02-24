2cc9dc0b2ab0237e73b6c1b0cddc40019838303f
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-24 19:54:38 +0100

    chore: add json extras lazyvim plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.rust" },
