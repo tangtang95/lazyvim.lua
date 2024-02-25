fdc09e5d91509f85a5c5b65a91715d3fafa27cee
Tangtang Zhou <5919707+tangtang95@users.noreply.github.com>
2024-02-25 19:16:10 +0100

    chore: fix lualine for root dir

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,5 @@
+local Util = require("lazyvim.util")
+
 return {
   {
     "nvimdev/dashboard-nvim",
@@ -14,6 +16,14 @@ return {
       projects.key_format = "  %s"
 
       table.insert(opts.config.center, 3, projects)
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      -- Replace first element of C block section by making sure that it is shown also when Lazy root is equal to cwd
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      opts.sections.lualine_c[1] = Util.lualine.root_dir({ cwd = true })
     end,
   },
 }
