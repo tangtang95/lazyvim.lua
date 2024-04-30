fbe41573b20adcd0f9e27cf2feecc6586b1f5d82
Tangtang Zhou <tangtang2995@gmail.com>
2024-04-30 22:04:04 +0200

    feat: update deps and add nix support

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,5 +1,5 @@
 return {
-  -- nushell parsing
+  -- nushell + nix support
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
@@ -8,8 +8,32 @@ return {
     },
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "nu" })
+        vim.list_extend(opts.ensure_installed, { "nu", "nix" })
       end
     end,
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "nil", "nixpkgs-fmt" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = {},
+      },
+    },
+  },
+{
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      nix = { "nixpkgs-fmt", "alejandra" },
+    },
+  },
+}
 }
