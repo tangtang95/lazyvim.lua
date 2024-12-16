c6dec6ade5f9ab8697504a07d45b4af60cd6343a
Tangtang Zhou <tangtang2995@gmail.com>
2024-12-16 18:38:08 +0100

    chore: use extras lang for nushell and nix support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
+    { import = "lazyvim.plugins.extras.lang.nushell" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,24 +1,4 @@
 return {
-  -- nushell + nix support
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      -- NOTE: additional parser
-      { "nushell/tree-sitter-nu" },
-    },
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "nu", "nix" })
-      end
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "nil", "nixpkgs-fmt" })
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
@@ -42,14 +22,5 @@ return {
         },
       },
     },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        nix = { "nixpkgs-fmt", "alejandra" },
-      },
-    },
-  },
+  }
 }
