4e70d85bc5c9ae11e337c663e307e47d86673589
Tangtang Zhou <tangtang2995@gmail.com>
2025-03-08 12:08:17 +0100

    chore: migrate to zig 0.14.0

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -9,7 +9,6 @@ return {
           settings = {
             zls = {
               enable_build_on_save = true,
-              build_on_save_step = "check",
             },
           },
         },
