741aa24484856e54cca272f247dcddd0cd769feb
Tangtang Zhou <tangtang2995@gmail.com>
2025-08-18 19:09:10 +0200

    chore: use only marksman for markdown

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,6 @@ require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -27,6 +27,7 @@ return {
           },
         },
         gopls = {},
+        marksman = {},
       },
     },
   },
