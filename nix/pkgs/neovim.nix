4657a38e78249a5184ed85d613e4440b30d94787
Tangtang Zhou <tang-tang.zhou.ext@siemens.com>
2025-09-01 14:10:32 +0000

    chore: bundle it via nix

---
+++ nix/pkgs/neovim.nix
@@ -0,0 +1,76 @@
+{
+  lib,
+  wrapNeovimUnstable,
+  neovim-unwrapped,
+  neovimUtils,
+  writeShellScript,
+  lua5_1,
+  luarocks,
+  clang,
+  pkg-config,
+  cargo,
+  statix,
+  manix,
+  buildFHSEnv,
+  bundled ? true,
+}: let
+  nvim = let
+    config = let
+      extraPackages = [
+        lua5_1
+        luarocks
+        clang
+        pkg-config
+        cargo
+
+        # LSP
+        statix
+        manix
+      ];
+    in
+      neovimUtils.makeNeovimConfig
+      {
+        withPython3 = false;
+        withRuby = false;
+        withNodeJs = false;
+
+        extraLuaPackages = p:
+          with p; [
+            magick
+          ];
+
+        inherit extraPackages;
+
+        customRC =
+          if bundled
+          then ''
+            set runtimepath^=${../../.}
+            source ${../../.}/init.lua
+          ''
+          else ''
+            set runtimepath^=~/.nvim
+            set runtimepath-=~/.config/nvim
+            source ~/.nvim/init.lua
+          '';
+      }
+      // {
+        wrapperArgs = [
+          "--prefix"
+          "PATH"
+          ":"
+          "${lib.makeBinPath extraPackages}"
+        ];
+      };
+  in
+    wrapNeovimUnstable neovim-unwrapped config;
+in
+  buildFHSEnv {
+    name = "nvim";
+    targetPkgs = pkgs: [
+      nvim
+    ];
+
+    runScript = writeShellScript "nvim-fhs.sh" ''
+      exec ${nvim}/bin/nvim "$@"
+    '';
+  }
