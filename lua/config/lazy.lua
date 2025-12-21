737cfadb87b718f3a71cac2e138171f636727773
Tangtang Zhou <tangtang2995@gmail.com>
2025-12-21 19:57:47 +0100

    chore: add harpoon2 plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
